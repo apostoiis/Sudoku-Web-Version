8947990f50e260fa74c0f6062745b4c19cf710ad
Nico Saueressig <nico@nicosaueressig.de>
2014-05-02 14:52:58 +0100

    put curr or puzzle in get slash back in place

--- sudoku.rb
+++ sudoku.rb
@@ -24,22 +24,22 @@ end
 
 def puzzle(sudoku, difficulty=5)
   sudoku.map do
-  	|number|
-  	rand(difficulty) == 1 ? 0 : number
+    |number|
+    rand(difficulty) == 1 ? 0 : number
   end
 end
 
 def box_order_to_row_order(cells)
   boxes = cells.each_slice(9).to_a
   (0..8).to_a.inject([]) {|memo,i|
-  first_box_index = i / 3 * 3
-  three_boxes = boxes[first_box_index, 3]
-  three_rows_of_three = three_boxes.map do |box|
-  row_number_in_a_box = i % 3
-  first_cell_in_the_row_index = row_number_in_a_box * 3
-  box[first_cell_in_the_row_index, 3]
-  end
-   memo += three_rows_of_three.flatten }
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box|
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+  memo += three_rows_of_three.flatten }
 end
 
 def generate_new_puzzle_if_necessary
@@ -54,20 +54,20 @@ def prepare_to_check_solution
   @check_solution = session[:check_solution]
   if @check_solution
     flash[:notice] = "Incorrect values are highlighted in yellow"
-    end
-    session[:check_solution] = nil
+  end
+  session[:check_solution] = nil
 end
 
 get '/' do
   prepare_to_check_solution
   generate_new_puzzle_if_necessary
-  @current_solution = session[:current_solution] #|| session[:puzzle]
+  @current_solution = session[:current_solution] || session[:puzzle]
   @solution = session[:solution]
   @puzzle = session[:puzzle]
- #  sudoku = random_sudoku
- #  session[:solution] = sudoku
-	# @current_solution = puzzle(sudoku)
-	erb :index
+  #  sudoku = random_sudoku
+  #  session[:solution] = sudoku
+  # @current_solution = puzzle(sudoku)
+  erb :index
 end
 
 post '/' do
@@ -79,18 +79,18 @@ post '/' do
 end
 
 get '/last-visit' do
-	"Previous visit to homepage: #{session[:last_visit]}"
+  "Previous visit to homepage: #{session[:last_visit]}"
 end
 
 get '/solution' do
   if session[:solution] == nil
-      erb :sloth
+    erb :sloth
   else
-  @puzzle = session[:puzzle]
-  @check_solution = session[:check_solution]
-  @solution = session[:solution]
-  @current_solution = session[:solution]
-  erb :index
+    @puzzle = session[:puzzle]
+    @check_solution = session[:check_solution]
+    @solution = session[:solution]
+    @current_solution = session[:solution]
+    erb :index
   end
 end
 
